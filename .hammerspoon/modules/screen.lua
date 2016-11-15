798c38cecf2bc0553106d2d5a6b597e2e9405b21
matrix <matrxi@local>
2016-11-15 14:18:10 +0800

    update scren.lua script to change focus

--- .hammerspoon/modules/screen.lua
+++ .hammerspoon/modules/screen.lua
@@ -5,6 +5,57 @@ local geometry = require "hs.geometry"
 local mouse = require "hs.mouse"
 local layout = require 'hs.layout'
 local alert = require 'hs.alert'
+local screen = require 'hs.screen'
+
+-- {{{ Multiple Screen Focus Switch
+local SwitchScreenKey = '.';
+--One hotkey should just suffice for dual-display setups as it will naturally
+--cycle through both.
+--A second hotkey to reverse the direction of the focus-shift would be handy
+--for setups with 3 or more displays.
+
+--Predicate that checks if a window belongs to a screen
+local function isInScreen(sc, win)
+    return win:screen() == sc
+end
+
+local function moveMouseToScreen(sc)
+    local pt = geometry.rectMidPoint(sc:fullFrame())
+    mouse.setAbsolutePosition(pt)
+end
+
+local function focusScreen(sc, moveMouse)
+    --Get windows within screen, ordered from front to back.
+    --If no windows exist, bring focus to desktop. Otherwise, set focus on
+    --front-most application window.
+    if not sc then return end
+
+    local windows = fnutils.filter(
+    window.orderedWindows(),
+    fnutils.partial(isInScreen, sc))
+    local windowToFocus = #windows > 0 and windows[1] or window.desktop()
+    windowToFocus:focus()
+
+    if moveMouse then moveMouseToScreen(sc) end
+end
+
+--Bring focus to next display/screen
+hotkey.bind({'ctrl','cmd'}, SwitchScreenKey, function ()
+    local focused = window.focusedWindow()
+    if not focused then return end
+    local sc = focused:screen()
+    if not sc then return end
+    focusScreen(window.focusedWindow():screen():next(), true)
+end)
+
+--Bring focus to previous display/screen
+hotkey.bind({'ctrl','cmd'}, SwitchScreenKey, function()
+    local focused = window.focusedWindow()
+    if not focused then return end
+    local sc = focused:screen()
+    if not sc then return end
+    focusScreen(window.focusedWindow():screen():previous(), true)
+end)
 
 ------------- Multiple Screen Layouts --------------- {{{
 -- find screen name from (Macbook - System Preferences - Displays - Color - Display profile)
